1acad744dccba1ae9126fcbc37c4ca4db3fb825f
Mike <mike.watson@gmail.com>
2024-12-09 22:49:05 -0500

    Very slow day 9 part 2 solution

--- 2024/day09.php
+++ 2024/day09.php
@@ -45,26 +45,67 @@ class Day09 extends Day
 
     public function part2()
     {
-    	$this->echoOn();
+        //$this->data = "2333133121414131402";
 
-    	$this->data = "2333133121414131402";
+        $fileId = 0;
 
-    	$fileId = 0;
+        $this->fs = [];
 
-    	$this->fs = [];
+        for ($i = 0; $i < strlen($this->data); $i++) {
+            $val = (int) $this->data[$i];
+            $fill = $i % 2 ? false : $fileId;
 
-    	for ($i = 0; $i < strlen($this->data); $i++) {
-    		$val = (int) $this->data[$i];
-    		$fill = $i % 2 ? false : $fileId;
+            for ($x = 0; $x < $val; $x++) {
+                $this->fs[] = $fill;
+            }
 
-    		
+            if (!($i % 2)) {
+                $fileId++;
+            }
+        }
 
-    		if (!($i % 2)) {
-    			$fileId++;
-    		}
-    	}
+        $end = count($this->fs) - 1;
 
-    	return 0;
+        $moved = [];
+
+        while (1) {
+            while ($this->fs[$end] === false) {
+                $end--;
+            }
+
+            [ $start, $len ] = $this->getLastFileInfo($end);
+            if ($start == -1) {
+                break;
+            }
+
+            if (!empty($moved[$this->fs[$start]])) {
+                $end = $start - 1;
+                continue;
+            }
+
+            $emptyStart = $this->findEmpty(0, $len);
+
+            if ($emptyStart > $start) {
+                $end = $start - 1;
+                continue;
+            }
+
+            if ($emptyStart !== false) {
+                $fileId = $this->fs[$end];
+                for ($i = $emptyStart; $i < $emptyStart + $len; $i++) {
+                    $this->fs[$i] = $fileId;
+                }
+                for ($i = $start; $i < $start + $len; $i++) {
+                    $this->fs[$i] = false;
+                }
+
+                $moved[$fileId] = true;
+            }
+
+            $end = $start - 1;
+        }
+
+        return $this->calcChecksum();
     }
 
     protected function calcChecksum()
@@ -80,7 +121,6 @@ class Day09 extends Day
     	return $checkSum;
     }
 
-    // this used to work for part 2 but I changed the implementatio
     protected function findEmpty($start, $minLength = 1)
     {
     	for ($i = $start; $i < count($this->fs); $i++) {
@@ -105,5 +145,19 @@ class Day09 extends Day
     	}
 
     	return false;
+    }
+
+    protected function getLastFileInfo($max)
+    {
+        $curId = $this->fs[$max];
+        $start = -1;
+        for ($i = $max; $i >= 0; $i--) {
+            if ($this->fs[$i] != $curId) {
+                $start = $i + 1;
+                break;
+            }
+        }
+
+        return [ $start, $max - $start + 1 ];
     }
 }
