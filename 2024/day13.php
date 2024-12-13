bd4a001ff117bc8b59146c8104efe6740339c413
Mike <mike.watson@gmail.com>
2024-12-13 17:27:47 -0500

    Solved both parts of day 13

--- 2024/day13.php
+++ 2024/day13.php
@@ -6,8 +6,9 @@ class Day13 extends Day
 
     public function part1()
     {
-    	/*
-    	$this->data = "Button A: X+94, Y+34
+        $this->pos = 0;
+
+        /*$this->data = "Button A: X+94, Y+34
 Button B: X+22, Y+67
 Prize: X=8400, Y=5400
 
@@ -21,8 +22,7 @@ Prize: X=7870, Y=6450
 
 Button A: X+69, Y+23
 Button B: X+27, Y+71
-Prize: X=18641, Y=10279";
-*/
+Prize: X=18641, Y=10279";*/
 
 		$tokens = 0;
 
@@ -33,6 +33,27 @@ Prize: X=18641, Y=10279";
 
 			//print_r($machine);
 
+            // I recognized there was a mathemetical solution to this one but I had no idea
+            // what to search for. this is cramer's rule application. I left in my original
+            // code which was partially working, but this is the solution I wanted to use.
+
+            $aPresses = (
+                ($machine['p']['x'] * $machine['b']['y']) - ($machine['p']['y'] * $machine['b']['x'])
+            ) / (
+                ($machine['a']['x'] * $machine['b']['y']) - ($machine['a']['y'] * $machine['b']['x'])
+            );
+
+            $bPresses = (
+                ($machine['a']['x'] * $machine['p']['y']) - ($machine['a']['y'] * $machine['p']['x'])
+            ) / (
+                ($machine['a']['x'] * $machine['b']['y']) - ($machine['a']['y'] * $machine['b']['x'])
+            );
+
+            if (is_int($aPresses) && is_int($bPresses)) {
+                $tokens += ($aPresses * 3) + $bPresses;
+            }
+
+            /*
 			$max = [ 'a' => 0, 'b' => 0 ];
 			foreach ([ 'a', 'b' ] as $ab) {
 				$xMax = floor($machine['p']['x'] / $machine[$ab]['x']);
@@ -111,6 +132,7 @@ Prize: X=18641, Y=10279";
 				$tokens += $aPresses * ($pref == 'a' ? 3 : 1);
 				$tokens += $bPressesX * ($pref == 'b' ? 3 : 1);
 			}
+            */
 		}
 
 		return $tokens;
@@ -118,9 +140,35 @@ Prize: X=18641, Y=10279";
 
 	public function part2()
 	{
+        $this->pos = 0;
+
+        $tokens = 0;
+
+        while ($machine = $this->getNextMachine(10000000000000)) {
+
+            // copy/paste
+
+            $aPresses = (
+                ($machine['p']['x'] * $machine['b']['y']) - ($machine['p']['y'] * $machine['b']['x'])
+            ) / (
+                ($machine['a']['x'] * $machine['b']['y']) - ($machine['a']['y'] * $machine['b']['x'])
+            );
+
+            $bPresses = (
+                ($machine['a']['x'] * $machine['p']['y']) - ($machine['a']['y'] * $machine['p']['x'])
+            ) / (
+                ($machine['a']['x'] * $machine['b']['y']) - ($machine['a']['y'] * $machine['b']['x'])
+            );
+
+            if (is_int($aPresses) && is_int($bPresses)) {
+                $tokens += ($aPresses * 3) + $bPresses;
+            }
+        }
+
+        return $tokens;
 	}
 
-    protected function getNextMachine()
+    protected function getNextMachine($prizeAdd = 0)
     {
         if (!is_array($this->data)) {
             $this->data = explode("\n", $this->data);
@@ -147,8 +195,8 @@ Prize: X=18641, Y=10279";
                 $machine[$button]['y'] = $matches[3];
             }
             if (preg_match('/^Prize: X=([0-9]+), Y=([0-9]+)$/', $inf, $matches)) {
-            	$machine['p']['x'] = $matches[1];
-            	$machine['p']['y'] = $matches[2];
+            	$machine['p']['x'] = $matches[1] + $prizeAdd;
+            	$machine['p']['y'] = $matches[2] + $prizeAdd;
             }
         }
 
